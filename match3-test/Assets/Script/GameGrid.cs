d147ca016b63b5a0b5375f11e67669db85aaa2b8
filipecocolo1 <53791311+filipecocolo1@users.noreply.github.com>
2020-08-23 01:51:52 -0300

    fazendo Mach

--- match3-test/Assets/Script/GameGrid.cs
+++ match3-test/Assets/Script/GameGrid.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEditorInternal;
-using UnityEngine;
+﻿
+using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices.WindowsRuntime;
-using System.Runtime.InteropServices;
+using UnityEditorInternal;
+using UnityEngine;
+using UnityEngine.UIElements;
+
 
 public class GameGrid : MonoBehaviour
 {
@@ -13,25 +14,28 @@ public class GameGrid : MonoBehaviour
     public float C_Widy;
     private RedeDeItem[,] _items;
     private RedeDeItem _itemAtualmenteAtivo;
+    public static int minitensForMACH = 3;
     // Start is called before the first frame update
     void Start()
     {
         GetComida();
         fillGrid();
         RedeDeItem.OnMouseOverItemEventHandler += OnMouseOverItem;
-        List<RedeDeItem> machForItemns = SearchVertically(_items[3,3]);
-        if (machForItemns.Count >= 3) {
+        List<RedeDeItem> machForItemns = SearchVertically(_items[3, 3]);
+        if (machForItemns.Count >= 3)
+        {
 
             Debug.Log("Há um match Valido para um indice 3x3");
-          
-        
+
+
         }
         else
-        { 
+        {
             Debug.Log("Não Ha um match valido para indice 3x3");
         }
     }
-    void OnDisable() {
+    void OnDisable()
+    {
 
         RedeDeItem.OnMouseOverItemEventHandler -= OnMouseOverItem;
     }
@@ -46,7 +50,72 @@ public class GameGrid : MonoBehaviour
             }
         }
     }
-   
+
+    //MachInfo GetMachInformation(RedeDeItem intem)
+    //{
+    //    MachInfo m = new MachInfo();
+    //    m.mach = null;
+    //    List<RedeDeItem> hMach = SearchHorizontally(intem);
+    //    List<RedeDeItem> vMach = SearchVertically(intem);
+    //    if (hMach.Count >= minitensForMACH && hMach.Count > vMach.Count)
+    //    {
+    //        m.machStartX = GetMinimumX(hMach);
+    //        m.machEndingX = GetMaximumX(hMach);
+    //        m.machStartY = m.machEndingY = hMach[0].y;
+
+    //        m.mach = hMach;
+    //        //Definir informação  para mach horizontal 
+
+    //    }
+    //    else if (vMach.Count >= minitensForMACH)
+    //    {
+    //        m.machStartY = GetMinimumY(vMach);
+    //        m.machEndingY = GetMaximumY(vMach);
+    //        m.machStartX= m.machEndingX = hMach[0].x;
+
+    //        m.mach = vMach;
+
+    //    }
+    //    return m;
+    //}
+    //int GetMinimumX(List<RedeDeItem> items)
+    //{
+    //    float[] indices = new float[items.Count];
+    //    for (int i = 0; i < indices.Length; i++)
+    //    {
+    //        indices[i] = items[i].x;
+    //    }
+    //    return (int)Mathf.Min(indices);
+    //}
+
+    //int GetMaximumX(List<RedeDeItem> items)
+    //{
+    //    float[] indices = new float[items.Count];
+    //    for (int i = 0; i < indices.Length; i++)
+    //    {
+    //        indices[i] = items[i].x;
+    //    }
+    //    return (int)Mathf.Max(indices);
+    //}
+    //int GetMinimumY(List<RedeDeItem> items)
+    //{
+    //    float[] indices = new float[items.Count];
+    //    for (int i = 0; i < indices.Length; i++)
+    //    {
+    //        indices[i] = items[i].y;
+    //    }
+    //    return (int)Mathf.Min(indices);
+    //}
+    //int GetMaximumY(List<RedeDeItem> items)
+    //{
+    //    float[] indices = new float[items.Count];
+    //    for (int i = 0; i < indices.Length; i++)
+    //    {
+    //        indices[i] = items[i].y;
+    //    }
+    //    return (int)Mathf.Max(indices);
+    //}
+
     void GetComida()
     {
         _comida = Resources.LoadAll<GameObject>("Prefabs");
@@ -61,11 +130,12 @@ public class GameGrid : MonoBehaviour
     RedeDeItem InstantieteComida(int x, int y)
     {
         GameObject randomcomidas = _comida[Random.Range(0, _comida.Length)];
+
         RedeDeItem newComida = ((GameObject)Instantiate(randomcomidas, new Vector3(x * C_Widy, y), Quaternion.identity)).GetComponent<RedeDeItem>();
         newComida.PosicaoDoItemAlterada(x, y);
         return newComida;
     }
-   
+
     void OnMouseOverItem(RedeDeItem item)
     {
         if (_itemAtualmenteAtivo == item)
@@ -84,10 +154,10 @@ public class GameGrid : MonoBehaviour
             if (xDiff + yDiff == 1)
             {
                 StartCoroutine(Swap(_itemAtualmenteAtivo, item));
-                
+
                 //Permetir Swap
             }
-           
+
             else
             {
                 Debug.LogError("Eles estão a mais de uma unidade longe um do outro");
@@ -136,7 +206,7 @@ public class GameGrid : MonoBehaviour
 
     }
 
-  List<RedeDeItem> SearchHorizontally(RedeDeItem item)
+    List<RedeDeItem> SearchHorizontally(RedeDeItem item)
     {
         List<RedeDeItem> hItemns = new List<RedeDeItem> { item };
         int left = item.x - 1;
@@ -150,29 +220,30 @@ public class GameGrid : MonoBehaviour
         {
             hItemns.Add(_items[right, item.y]);
             right++;
-           
+
         }
 
         return hItemns;
-    
-  }
-    List<RedeDeItem> SearchVertically (RedeDeItem item)
+
+    }
+    List<RedeDeItem> SearchVertically(RedeDeItem item)
     {
         List<RedeDeItem> vItemns = new List<RedeDeItem> { item };
         int lower = item.y - 1;
         int upper = item.y + 1;
-        while (lower>=0 && _items[item.x,lower].id== item.id)
+        while (lower >= 0 && _items[item.x, lower].id == item.id)
         {
             vItemns.Add(_items[item.x, lower]);
             lower--;
-             
+
         }
-        while (upper < ySize && _items[item.x, upper].id == item.id) {
+        while (upper < ySize && _items[item.x, upper].id == item.id)
+        {
             vItemns.Add(_items[item.x, upper]);
             upper++;
-        
+
         }
-        return vItemns; 
+        return vItemns;
     }
 
 
---
+++ match3-test/Assets/Script/MachInfo.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+public class MachInfo {
+
+    public List<RedeDeItem> mach;
+    public int machStartX;
+    public int machEndingX;
+    public int machStartY;
+    public int machEndingY;
+
+    public bool validMach {
+        get {return  mach !=null;}
+    
+    
+    
+     }
+
+
+}
+
